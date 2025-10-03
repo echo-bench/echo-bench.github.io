0844ef47163f183ba1f0845a69a97ece126f6e64
g-luo <graceluo@berkeley.edu>
2025-10-03 11:00:59 -0700

    Add updated figures

--- static/js/interactions.js
+++ static/js/interactions.js
@@ -3,42 +3,18 @@ function playVideo(id) {
   video.play();
 }
 
-function changeVideo(id) {
-  const selectParent = document.getElementById('select-' + id);
-  if (selectParent === null) {
-      return ;
-  }
-  let path0 = selectParent.getElementsByTagName('select')[0].selectedOptions[0];
-  let path1 = selectParent.getElementsByTagName('select')[1].selectedOptions[0];
-  path0 = path0.textContent.trim().toLowerCase();
-  path0 = path0.split(/[\s-]+/)[0] || '';
-  path1 = path1.textContent.trim().toLowerCase();
-
-  const video = document.getElementById(id);
-  const videoSrc = video.querySelector('source').src;
-  const videoPath = videoSrc.split("/");
-  const videoBase = videoPath[videoPath.length-1].replace(".m4v", "").split("_");
-  let newVideoPath = videoPath.slice(0, -1).join("/") + "/";
-
-  newVideoPath += path0 + "_" + path1 + ".m4v";
-  video.src = newVideoPath;
-  video.load();
-}
-
 function changeImage(id) {
   const selectParent = document.getElementById('select-' + id);
-  if (selectParent === null) {
-      return ;
-  }
-  let path0 = selectParent.getElementsByTagName('select')[0].selectedOptions[0].id;
-  let path1 = selectParent.getElementsByTagName('select')[1].selectedOptions[0].id;
+  if (!selectParent) return;
+
+  let path0 = selectParent.getElementsByTagName('select')[0].value;
 
   const image = document.getElementById(id);
   const imageSrc = image.src;
   let imagePath = imageSrc.split("/");
   imagePath = imagePath.slice(0, -1).join("/") + "/";
 
-  image.src = imagePath + path0 + "_" + path1 + ".png";
+  image.src = imagePath + path0 + ".jpg";
 }
 
 function changePlotly(id) {
