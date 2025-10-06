847bda3340e0f51ab74b9578cdeb78b3c3fcf20d
g-luo <graceluo@berkeley.edu>
2025-10-06 01:27:41 -0700

    Add WIP specialized metrics

--- static/js/interactions.js
+++ static/js/interactions.js
@@ -3,19 +3,6 @@ function playVideo(id) {
   video.play();
 }
 
-function changeImage(id) {
-  const selectParent = document.getElementById('select-' + id);
-  if (!selectParent) return;
-
-  let path0 = selectParent.getElementsByTagName('select')[0].value;
-
-  const image = document.getElementById(id);
-  const imageSrc = image.src;
-  let imagePath = imageSrc.split("/");
-  imagePath = imagePath.slice(0, -1).join("/") + "/";
-  image.src = imagePath + path0 + ".jpg";
-}
-
 function changePlotly(id) {
   const selectEl = document.querySelector('#select-' + id + ' select');
   if (!selectEl) return;
@@ -137,4 +124,6 @@ async function populateGallery(galleryId = 'gallery') {
   }
 }
 
-document.addEventListener('DOMContentLoaded', () => populateGallery());
+document.addEventListener('DOMContentLoaded', () => {
+  populateGallery();
+});
